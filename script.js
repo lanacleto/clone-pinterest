4468dae7a0a6d178d185feac0b92200b193fdc48
Luiz Fernando Anacl <luizfernandoanacleto68@gmail.com>
2020-10-23 09:36:45 -0300

    hr

---
+++ script.js
@@ -0,0 +1,17 @@
+const grids = document.querySelectorAll(".grid");
+const headings = document.querySelectorAll(".heading .wrapper .text");
+
+function enterScreen(index) {}
+function exitScreen(index, exitDelay) {}
+
+function setupAnimationCycle({
+  initialScreenIndex,
+  timePerScreen,
+  exitDelay,
+}) {}
+
+setupAnimationCycle({
+  initialScreenIndex: 0,
+  timePerScreen: 2000,
+  exitDelay: 200 * 7,
+});
